bbfa991f04cc0912ab48bbc25d3436f781c40388
FatihBc <fatihbuyukcam@gmail.com>
2025-11-19 21:45:00 +0300

    delete

--- backend/src/seeders/000_cleanup.js
+++ backend/src/seeders/000_cleanup.js
@@ -1,3 +1,6 @@
 exports.seed = async function (knex) {
-  knex.raw("TRUNCATE TABLE users, products CASCADE");
+  knex.raw("TRUNCATE TABLE doctor, products CASCADE");
+  knex.raw("TRUNCATE TABLE location, products CASCADE");
+  knex.raw("TRUNCATE TABLE research, products CASCADE");
+  knex.raw("TRUNCATE TABLE department, products CASCADE");
 };
--- backend/src/seeders/006_department_doctor.js
+++ backend/src/seeders/006_department_doctor.js
@@ -1,5 +1,5 @@
 exports.seed = async function (knex) {
-  await knex("department_doctor").del();
+  await knex("department_doctor").delete();
   await knex("department_doctor").insert([
     { department_id: 1, doctor_id: 1 },
     { department_id: 1, doctor_id: 2 },
