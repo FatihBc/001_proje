7abe7ad90ed8936a5a3b6160a634d9076ebacc0e
FatihBc <fatihbuyukcam@gmail.com>
2025-11-19 20:22:56 +0300

    Update doctorRoutes.js

--- backend/src/routes/doctorRoutes.js
+++ backend/src/routes/doctorRoutes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const doctorController = require("../controllers/doctorController");
 
+router.get("/", doctorController.getDoctors);
+
 router.get("/filter", doctorController.getDoctors);
 router.post("/", doctorController.createDoctor);
 router.put("/:id", doctorController.updateDoctor);
