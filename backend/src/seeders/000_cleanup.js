90244ea366002018ba9fbde455504cc81bbd806a
FatihBc <fatihbuyukcam@gmail.com>
2025-11-19 21:51:23 +0300

    Update 000_cleanup.js

--- backend/src/seeders/000_cleanup.js
+++ backend/src/seeders/000_cleanup.js
@@ -1,6 +1,6 @@
 exports.seed = async function (knex) {
-  await knex.raw("TRUNCATE TABLE doctor CASCADE");
-  await knex.raw("TRUNCATE TABLE location CASCADE");
-  await knex.raw("TRUNCATE TABLE research CASCADE");
-  await knex.raw("TRUNCATE TABLE department CASCADE");
+  await knex.raw("ALTER SEQUENCE department_department_id_seq RESTART WITH 1");
+  await knex.raw("ALTER SEQUENCE location_location_id_seq RESTART WITH 1");
+  await knex.raw("ALTER SEQUENCE doctor_doctor_id_seq RESTART WITH 1");
+  await knex.raw("ALTER SEQUENCE research_research_id_seq RESTART WITH 1");
 };
