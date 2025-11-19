fb31a82a06556ccc196c0b56b00af076bbc48832
FatihBc <fatihbuyukcam@gmail.com>
2025-11-19 22:15:05 +0300

    Update knex.js

--- backend/src/db/knex.js
+++ backend/src/db/knex.js
@@ -1,6 +1,9 @@
 const knex = require("knex");
 const config = require("../../knexfile");
 
-const db = knex(config[process.env.NODE_ENV || "development"]);
+const environment = process.env.NODE_ENV || "development";
+console.log("Knex environment:", environment);
+
+const db = knex(config[environment]);
 
 module.exports = db;
