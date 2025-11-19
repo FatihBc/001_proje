9693cf3f9341f149f24587cbf752b9dec0bd9432
FatihBc <fatihbuyukcam@gmail.com>
2025-11-19 20:45:14 +0300

    Update 000_cleanup.js

--- backend/src/seeders/000_cleanup.js
+++ backend/src/seeders/000_cleanup.js
@@ -2,7 +2,7 @@ const cleaner = require("knex-cleaner");
 
 exports.seed = async function (knex) {
   await cleaner.clean(knex, {
-    mode: "truncate",
+    mode: "delete",
     ignoreTables: ["knex_migrations", "knex_migrations_lock"],
   });
 };
