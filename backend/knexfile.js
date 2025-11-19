0f1384fef7407dcaaa4fee1e9e4ccefa62a47f7f
FatihBc <fatihbuyukcam@gmail.com>
2025-11-19 22:09:31 +0300

    env changed

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -17,10 +17,7 @@ module.exports = {
 
   production: {
     client: "pg",
-    connection: {
-      connectionString: process.env.DATABASE_URL,
-      ssl: { rejectUnauthorized: false },
-    },
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: "./src/migrations",
     },
