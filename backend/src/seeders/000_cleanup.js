e3317dce52b0489779fa3eee259593caefef5121
FatihBc <fatihbuyukcam@gmail.com>
2025-11-19 21:10:46 +0300

    Update 000_cleanup.js

--- backend/src/seeders/000_cleanup.js
+++ backend/src/seeders/000_cleanup.js
@@ -1,8 +1,15 @@
 const cleaner = require("knex-cleaner");
 
 exports.seed = async function (knex) {
+  // Önce tabloları temizle
   await cleaner.clean(knex, {
     mode: "delete",
     ignoreTables: ["knex_migrations", "knex_migrations_lock"],
   });
+
+  // Sonra sequence reset
+  await knex.raw("ALTER SEQUENCE doctors_doctor_id_seq RESTART WITH 1");
+  await knex.raw("ALTER SEQUENCE departments_department_id_seq RESTART WITH 1");
+  await knex.raw("ALTER SEQUENCE locations_location_id_seq RESTART WITH 1");
+  await knex.raw("ALTER SEQUENCE research_research_id_seq RESTART WITH 1");
 };
