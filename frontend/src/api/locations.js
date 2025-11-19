242f3657c1dcf4a70154c5605fab9e3b1af5fd78
FatihBc <fatihbuyukcam@gmail.com>
2025-11-19 22:58:29 +0300

    Update locations.js

--- frontend/src/api/locations.js
+++ frontend/src/api/locations.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:4000/api";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export async function getLocations() {
   const res = await axios.get(`${API_URL}/locations`);
