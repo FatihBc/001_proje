0274d989089e96b3598d13077f66304eeb318def
FatihBc <fatihbuyukcam@gmail.com>
2025-11-23 11:38:56 +0300

    Knexcleaner and sqlite3 removed,

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,23 +1,12 @@
 require("dotenv").config();
 
 module.exports = {
-  development: {
-    client: "sqlite3",
-    connection: {
-      filename: "./src/db/dev.sqlite3",
-    },
-    useNullAsDefault: true,
-    migrations: {
-      directory: "./src/migrations",
-    },
-    seeds: {
-      directory: "./src/seeders",
-    },
-  },
-
   production: {
     client: "pg",
-    connection: process.env.DATABASE_URL,
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    },
     migrations: {
       directory: "./src/migrations",
     },
